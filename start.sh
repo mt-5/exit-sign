c7e69632f9fea3de7b1785e09d874ff793d4fcf4
Maciej Toborek <maciej@toborek.com>
2024-04-22 22:19:50 +0100

    updated demo script

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+DIR="$(dirname "$(realpath "$0")")"
 
 echo -e 'Demo time'
 
-sudo ./display/matrix/example-api-use/demo -D1 runtext.ppm  --led-gpio-mapping=adafruit-hat --led-rows=32 --led-cols=64 --led-chain=3
+cd "$DIR/display/matrix/examples-api-use"
+sudo ./demo -D1 runtext.ppm  --led-gpio-mapping=adafruit-hat --led-rows=32 --led-cols=64 --led-chain=3
